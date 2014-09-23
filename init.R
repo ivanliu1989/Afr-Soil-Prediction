cb4b80cf883a7d61e4926f22e0128d607faf5f0e
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-23 15:29:22 +1000

    initializing

---
+++ init.R
@@ -0,0 +1,7 @@
+##### setup project #####
+setwd('/Users/ivan/Work_directory/Afr-Soil-Prediction')
+library(caret)
+
+##### data exploration #####
+train <- read.csv('data/training.csv', stringsAsFactor=F)
+test <- read.csv('data/sorted_test.csv', stringsAsFactor=F)
