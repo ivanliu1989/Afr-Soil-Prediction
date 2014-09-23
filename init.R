5bf0511b3a788cd3c684ac599534e937bd1c2851
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-23 15:36:29 +1000

    initializing

--- init.R
+++ init.R
@@ -5,3 +5,9 @@ library(caret)
 ##### data exploration #####
 train <- read.csv('data/training.csv', stringsAsFactor=F)
 test <- read.csv('data/sorted_test.csv', stringsAsFactor=F)
+dim(train)
+# set y
+y <- c('Ca', 'P', 'pH', 'SOC', 'Sand')
+head(train[,y])
+# pca 
+
