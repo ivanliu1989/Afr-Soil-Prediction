6bdd4900f66b3197026eea98fda09a29cbb47161
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-25 17:34:07 +1000

    first try

--- first submission.R
+++ first submission.R
@@ -1,3 +1,17 @@
-##### setup project #####
+###########
+## setup ##
+###########
 setwd('/Users/ivan/Work_directory/Afr-Soil-Prediction')
 library(caret)
+train <- read.csv('data/training.csv', stringsAsFactor=F)
+test <- read.csv('data/sorted_test.csv', stringsAsFactor=F)
+dim(train)
+
+###################
+## Data Cleaning ##
+###################
+y <- c('Ca', 'P', 'pH', 'SOC', 'Sand')
+train_y <- train[,y]
+train_x <- train[,-which(names(train) %in% y)]
+names(train)
+rm_index <- c(m2379.76:m2352.76)
