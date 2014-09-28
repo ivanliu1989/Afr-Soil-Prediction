d86000c1e188c4bce6c21b85434f7d1efbf4a70e
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-28 13:12:42 +1000

    r try

--- kaggle/svm2.0.r
+++ kaggle/svm2.0.r
@@ -1,5 +1,5 @@
 setwd("C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Afr-Soil-Prediction")
-library(e1071)
+library(e1071); library(caret)
 
 train <- read.csv("Data/training.csv",header=TRUE,stringsAsFactors=FALSE)
 test <- read.csv("Data/sorted_test.csv",header=TRUE,stringsAsFactors=FALSE)
@@ -7,16 +7,34 @@ test <- read.csv("Data/sorted_test.csv",header=TRUE,stringsAsFactors=FALSE)
 submission <- test[,1]
 
 labels <- train[,c("Ca","P","pH","SOC","Sand")]
+index <- createDataPartition(y = train$Ca, p=0.8, list=F)
 
 # Exclude CO2
 train <- train[,c(2:2655,2671:3579)]
 test <- test[,c(2:2655,2671:3579)]
 
-svms <- lapply(1:ncol(labels),
+# split
+train_train <- train[index,]
+train_test <- train[-index,]
+labels_train <- labels[index,]
+labels_test <- labels[-index,]
+
+svms <- lapply(1:ncol(labels_train),
                function(i)
                {
-                 svm(train,labels[,i],cost=10000,scale=FALSE)
+                 svm(train_train,labels_train[,i],cost=10000,scale=FALSE)
                })
+pred_Ca <- predict(svms[[1]], train_train)
+
+
+
+
+
+
+
+
+
+
 
 predictions <- sapply(svms,predict,newdata=test)
 
