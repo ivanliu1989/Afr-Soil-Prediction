b364eaa9bea2217a0faad080e2618c4f5da1947b
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-29 16:36:10 +1000

    dealing with p

--- 3_modeling.R
+++ 3_modeling.R
@@ -1,3 +1,6 @@
+####################
+## pre Processing ##
+####################
 setwd('/Users/ivan/Work_directory/test/Afr-Soil-Prediction')
 require(caret)
 load('data/2_feature_engineer.RData')
@@ -18,51 +21,94 @@ names(test)[1] <- "PIDN"
 ID <- as.data.frame(test$PIDN)
 colnames(ID)[1]<-"PIDN"
 
-################
-## 1.train_Ca ##
-################
+########################
+## Parallel computing ##
+########################
+library(doMC)
+registerDoMC(cores = 5)
+
+############################
+## Model control & tuning ##
+############################
     # Grid <- expand.grid(C=c(8,16,32,64,128),sigma=c(0.0118)) 
 fitControl <- trainControl(method="adaptive_cv",number=10,
-                           repeats=10, summaryFunction = defaultSummary,
+                           repeats=5, summaryFunction = defaultSummary,
                            returnResamp = "all",
-                           adaptive=list(min=12,
+                           adaptive=list(min=10,
                                          alpha=.05,
                                          method='BT',
                                          complete=T))
 
+################
+## 1.train_Ca ##
+################
 fit_Ca_svm <- train(Ca~., data=train_Ca, 
-                method='svmRadial',
+                method='svmPoly',
                 trControl = fitControl,
                 preProc = c('center','scale'),
                 tuneLength=10,
-                #tuneGrid = Grid,
+                # tuneGrid = Grid,
                 verbose=T, 
                 metric='RMSE')
-trellis.par.set(caretTheme())
-plot(fit_Ca)
-png('fit_Ca_svm.png')
+fit_Ca_svmPoly <- fit_Ca_svm
+png('fit_Ca_svmPoly.png') # visualize model performance
 trellis.par.set(caretTheme())
 plot(fit_Ca_svm)
 dev.off()
-Ca <- predict(fit_Ca_svm, test)
+Ca <- predict(fit_Ca_svm, test) # joint prediction results
 submit_Ca <- cbind(ID, Ca)
+
 ###############
 ## 2.train_P ##
 ###############
-fit_P_svm <- train(P~., data=train_P, 
-                    method='gbm',
+fit_P_gamboost <- train(P~., data=train_P, 
+                     method='gamboost',
+                     trControl = fitControl,
+                     preProc = c('center','scale'),
+                     tuneLength=10,
+                     # tuneGrid = Grid,
+                     verbose=T, 
+                     metric='RMSE')
+fit_P_avNNet <- train(P~., data=train_P, 
+                      method='avNNet',
+                      trControl = fitControl,
+                      preProc = c('center','scale'),
+                      tuneLength=10,
+                      # tuneGrid = Grid,
+                      verbose=T, 
+                      metric='RMSE')
+fit_P_ridge <- train(P~., data=train_P, 
+                    method='ridge',
                     trControl = fitControl,
                     preProc = c('center','scale'),
                     tuneLength=10,
-                   # tuneGrid = Grid,
+                    # tuneGrid = Grid,
                     verbose=T, 
                     metric='RMSE')
-png('fit_P_gbm.png')
+fit_P_lasso <- train(P~., data=train_P, 
+                     method='lasso',
+                     trControl = fitControl,
+                     preProc = c('center','scale'),
+                     tuneLength=10,
+                     # tuneGrid = Grid,
+                     verbose=T, 
+                     metric='RMSE')
+fit_P_glmnet <- train(P~., data=train_P, 
+                     method='glmnet',
+                     trControl = fitControl,
+                     preProc = c('center','scale'),
+                     tuneLength=10,
+                     # tuneGrid = Grid,
+                     verbose=T, 
+                     metric='RMSE')
+
+png('fit_P_gbm.png') # visualize model performance
 trellis.par.set(caretTheme())
 plot(fit_P_svm)
 dev.off()
-P <- predict(fit_P_svm, test)
+P <- predict(fit_P_svm, test) # joint prediction results
 submit_P <- cbind(submit_Ca, P)
+
 ################
 ## 3.train_pH ##
 ################
@@ -71,24 +117,34 @@ fit_pH_svm <- train(pH~., data=train_pH,
                     trControl = fitControl,
                     preProc = c('center','scale'),
                     tuneLength=10,
-                    #tuneGrid = Grid,
+                    # tuneGrid = Grid,
                     verbose=T, 
                     metric='RMSE')
-pH <- predict(fit_pH_svm, test)
+png('fit_pH_svm.png') # visualize model performance
+trellis.par.set(caretTheme())
+plot(fit_pH_svm)
+dev.off()
+pH <- predict(fit_pH_svm, test) # joint prediction results
 submit_pH <- cbind(submit_P, pH)
+
 #################
 ## 4.train_SOC ##
 #################
 fit_SOC_svm <- train(SOC~., data=train_SOC, 
-                   method='svmLinear',
-                   trControl = fitControl,
-                   preProc = c('center','scale'),
-                   tuneLength=10,
-                    #tuneGrid = Grid,
-                   verbose=T, 
-                   metric='RMSE')
-SOC <- predict(fit_SOC_svm, test)
+                    method='svmLinear',
+                    trControl = fitControl,
+                    preProc = c('center','scale'),
+                    tuneLength=10,
+                    # tuneGrid = Grid,
+                    verbose=T, 
+                    metric='RMSE')
+png('fit_SOC_svm.png') # visualize model performance
+trellis.par.set(caretTheme())
+plot(fit_SOC_svm)
+dev.off()
+SOC <- predict(fit_SOC_svm, test) # joint prediction results
 submit_SOC <- cbind(submit_pH, SOC)
+
 ##################
 ## 5.train_Sand ##
 ##################
@@ -100,9 +156,15 @@ fit_Sand_svm <- train(Sand~., data=train_Sand,
                      #tuneGrid = Grid,
                      verbose=T, 
                      metric='RMSE')
-Sand <- predict(fit_Sand_svm, test)
+png('fit_Sand_svm.png') # visualize model performance
+trellis.par.set(caretTheme())
+plot(fit_Sand_svm)
+dev.off()
+Sand <- predict(fit_Sand_svm, test) # joint prediction results
 submit_Final <- cbind(submit_SOC, Sand)
 
-
-save(fit_Ca_svm,fit_P_gbm,fit_pH_svm,fit_SOC_svm,fit_Sand_svm,file='models_2.RData')
+#################################
+## Save models and predictions ##
+#################################
+save(fit_Ca_svmPoly,file='models_2.RData')
 write.csv(submit_Final, 'Third_try_not_optimal_MAC_version.csv',row.names=F)
