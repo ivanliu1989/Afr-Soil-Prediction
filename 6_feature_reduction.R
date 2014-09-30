113cdcce54313eaadbe955e9d312a19bc76114c8
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-01 09:30:01 +1000

    feature reduction

--- 6_feature_reduction.R
+++ 6_feature_reduction.R
@@ -17,6 +17,19 @@ png('varImp_pH.png'); plot(varImp_pH, top=100); dev.off();
 varImp_Sand <- varImp(fit_Sand_svm, scale = FALSE)
 png('varImp_Sand.png'); plot(varImp_Sand, top=100); dev.off();
 
+train <- read.csv("data/training.csv",header=TRUE,stringsAsFactors=FALSE)
+test <- read.csv("data/sorted_test.csv",header=TRUE,stringsAsFactors=FALSE)
+    labels <- train[,c("Ca","P","pH","SOC","Sand")] # y
+    depth <- train$Depth # Depth-train
+    depth_test <- test$Depth # Depth-test
+    ID_test <- test[,1] # test-ID
+# Exclude CO2 and ID
+rm_index <- c(1, 2656:2670, 3595:3600) 
+rm_index2 <- c(1, 2656:2670, 3595) 
+train <- train[,-rm_index]
+test <- test[,-rm_index2]
+dim(test);dim(train);identical(names(train), names(test))
+
 ###################################
 ## Principal Components Analysis ##
 ###################################
