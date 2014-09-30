2df1070efa6e8838a8bcad49f643b2e1a4fc7fa2
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-01 09:39:53 +1000

    linearity test

--- 6_feature_reduction.R
+++ 6_feature_reduction.R
@@ -29,11 +29,53 @@ rm_index2 <- c(1, 2656:2670, 3595)
 train <- train[,-rm_index]
 test <- test[,-rm_index2]
 dim(test);dim(train);identical(names(train), names(test))
+total_data <- rbind(train,test); dim(total_data)
+set.seed(888)
+
+#######################################
+## Identifying correlated predictors ##
+#######################################
+descrCor <- cor(total_data)
+summary(descrCor[upper.tri(descrCor)])
+highlyCorDescr <- findCorrelation(descrCor, cutoff = 1)
+filteredDescr <- total_data[, -highlyCorDescr]
+descrCor2 <- cor(filteredDescr)
+summary(descrCor2[upper.tri(descrCor2)])
+dim(filteredDescr)
+# modeling
+train_linear <- cbind(Ca=labels$Ca, filteredDescr[1:1157,], Depth=depth)
+test_linear <- cbind(PIDN=ID_test,filteredDescr[1158:1884,], Depth=depth_test)
+dim(train_linear); dim(test_linear)
+fitControl <- trainControl(method="adaptive_cv",number=10,
+                           repeats=5, summaryFunction = defaultSummary,
+                           returnResamp = "all",
+                           adaptive=list(min=10,
+                                         alpha=.05,
+                                         method='BT',
+                                         complete=T))
+fit_Ca_svm <- train(Ca~., data=train_linear, 
+                    method='svmRadial',
+                    trControl = fitControl,
+                    preProc = c('center','scale'),
+                    tuneLength=10,
+                    # tuneGrid = Grid,
+                    verbose=T, 
+                    metric='RMSE')
+
+#########################
+## Linear dependencies ##
+#########################
+comboInfo <- findLinearCombos(total_data)
+comboInfo$remove
+total_data_linear <- total_data[,-comboInfo$remove]
+dim(total_data_linear)
 
 ###################################
 ## Principal Components Analysis ##
 ###################################
-
+preProcPCA <- preProcess(total_data, method='pca',thresh = 1)
+total_data_PCA <- predict(preProcPCA, total_data)
+head(total_data_PCA)
 
 #####################
 ## Factor Analysis ##
