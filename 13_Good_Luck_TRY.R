ae4c9657fa8725d1eb99a8e01340c789821cc441
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-03 10:15:15 +1000

    update

--- 13_Good_Luck_TRY.R
+++ 13_Good_Luck_TRY.R
@@ -10,15 +10,17 @@ train_P_2 <- train_P[-index_P,]
 ### Model preProcess ###
 set.seed(888)
 # Grid <- expand.grid(C=c(8,16,32,64,128),sigma=c(0.0118)) 
-fitControl <- trainControl(method="adaptive_cv",number=15,
-                           repeats=15, summaryFunction = defaultSummary,
+fitControl <- trainControl(method="adaptive_cv",number=10,
+                           repeats=5, summaryFunction = defaultSummary,
                            returnResamp = "all",
-                           adaptive=list(min=15,alpha=.01,method='BT',complete=T))
-fit_P_svm <- train(P~., data=train_P, method='svmRadial',trControl = fitControl,
-                   preProc = c('center','scale'),tuneLength=15,# tuneGrid = Grid,
+                           adaptive=list(min=10,alpha=.01,method='BT',complete=T))
+fit_P_svm <- train(P~., data=train_P_1, method='svmRadial',trControl = fitControl,
+                   preProc = c("YeoJohnson"),tuneLength=12,# tuneGrid = Grid,
                    verbose=T,metric='RMSE')
 
 ### log transformation ###
+range(train_P[,-3580])
+plot(train_P$P, train_P[,3000])
 
 ### Model evaluation ### 
 trellis.par.set(caretTheme())
@@ -27,3 +29,16 @@ Ca <- predict(fit_Ca_svm_pre, test)
 rmse(Sand, train_Sand$Sand)
 svmImp_P <- varImp(fit_P_svm, scale = FALSE) # varImp
 svmImp_P; plot(svmImp_P)
+
+### svm ###
+require(e1071)
+fit <- svm(P~., data=train_P_1, scale=1, type='eps-regression',
+           kernel='linear')
+P <- predict(fit, train_P_2)
+rmse(P, train_P_2$P)
+
+### transformation ### 
+range(train_P$P)
+shapiro.test(log10(train_P$P+0.5))[1]
+apply(train_P[,-3580], 2, shapiro.test)
+plot(density(log10(train_P$P+0.5)))
