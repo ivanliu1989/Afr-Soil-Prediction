ba64ca7ba71a98987869526403ffc0cbaf4ec633
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-10-06 19:18:08 +1100

    test

--- 17_bash_svm.R
+++ 17_bash_svm.R
@@ -1,4 +1,4 @@
-setwd('/Users/ivan/Work_directory/Afr-Soil-Prediction-master')
+setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Afr-Soil-Prediction')
 require(caret); require(hydroGOF); require(parcor); require(prospectr)
 load('data/datasets_all_01Oct2014.RData')
 ID <- as.data.frame(test[,1])
@@ -13,7 +13,7 @@ set.seed(888)
 fitControl <- trainControl(method="adaptive_cv",number=10,
                            repeats=5, summaryFunction = defaultSummary,
                            returnResamp = "all",
-                           adaptive=list(min=12,alpha=.05,method='BT',complete=T))
+                           adaptive=list(min=13,alpha=.05,method='BT',complete=T))
 fit_pH <- train(pH~., data=train_pH_1, method='svmRadial',trControl = fitControl,
                       preProc = c('center', 'scale'),tuneLength=10,
                       verbose=T,metric='RMSE')
@@ -22,5 +22,6 @@ pH <- predict(fit_pH, train_pH_1)
 pH2 <- predict(fit_pH, train_pH_2)
 rmse(pH, train_pH_1$pH)
 rmse(pH2, train_pH_2$pH)
+fit_pH
 
 save(fit_pH, file='models/fit_pH_baseline.RData')
