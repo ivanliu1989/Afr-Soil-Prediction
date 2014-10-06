3bec3e31af43b1675f6f0bb51f39a51f39d59d83
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-06 11:13:22 +1100

    update

--- 16_svm.R
+++ 16_svm.R
@@ -1,4 +1,4 @@
-setwd('H:\\Machine Learning\\Afr-Soil-Prediction')
+setwd('/Users/ivan/Work_directory/Afr-Soil-Prediction-master')
 require(caret); require(hydroGOF); require(parcor); require(prospectr)
 load('data/datasets_all_01Oct2014.RData')
 ID <- as.data.frame(test[,1])
@@ -7,8 +7,8 @@ index_P <- createDataPartition(train_P$P, p=0.75, list = F)
 train_P_1 <- train_P[index_P,]
 train_P_2 <- train_P[-index_P,]
 
-y <- train_P$P;
-x <- train_P[,-c(1,3580)];
+y <- train_Ca$Ca;
+x <- train_Ca[,-c(1)];
 # x$Depth <- as.numeric(x$Depth)
 x <- as.matrix(x)
 y<-as.matrix(y)
@@ -17,13 +17,13 @@ library('e1071')
 fit_P_svmL <- svm(x,y,scale=T,kernel='linear',cost=100, cross=10, cachesize=1000, tolerance=.001)
 fit_P_svmL$SV
 fit_P_svmL$coefs
-fit_P_svmL_tune <- tune.svm(P~., data=train_P, gamma=2^(-1:1),cost=2^(2:16))
+fit_P_svmL_tune <- tune.svm(Ca~., data=train_Ca, gamma=2^(-1:1),cost=2^(2:6))
 
-tc <- tune.control(nrepeat=10,repeat.aggregate=min, sampling='cross',sampling.aggregate=min,
+tc <- tune.control(nrepeat=1,repeat.aggregate=min, sampling='cross',sampling.aggregate=min,
                    sampling.dispersion=sd,cross=10,best.model=T,
                    performances=T,error.fun=NULL)
 
-fit_P_svmL_tune <- tune(svm, P~., data=train_P, ranges=list(gamma=2^(-1:1),cost=2^(2:13)),
+fit_P_svmL_tune <- tune(svm, Ca~., data=train_Ca, ranges=list(gamma=2^(-1:1),cost=2^(2:8)),
                         tunecontrol = tc)
 best.tune()
 
