ce2a6599557fb6c50c9b0a8a1676e499a9ca8536
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-08 09:44:53 +1100

    preprocess

--- 22_first_second_derivatives.R
+++ 22_first_second_derivatives.R
@@ -17,14 +17,32 @@ all.spatial <- rbind(train.spatial,test.spatial)
     
 # data for first derivative
 total.dev1 <- rbind(train[,-c(1,spatial.col)], test[,-c(1,spatial.col2)])
+png('dev_smoo.png')
+par(mfcol = c(1,1))
+plot(total.dev1[,500], type='l')
+plot(total.dev1[,1500], type='l')
+plot(total.dev1[,2500], type='l')
+plot(total.dev1[,3500], type='l')
+dev.off()
 
 ######################
-## First Derivative ##
+## First & Second Derivative ##
 ######################
+d1 <- t(diff(t(total.dev1), differences = 1)) # first derivative
+d2 <- t(diff(t(total.dev1), differences = 2)) # second derivative
+gd1 <- t(diff(t(total.dev1), differences = 1, lag = 10)) # first derivative with a gap of 10 bands
+gsd1 <- gapDer(X = total.dev1, m = 1, w = 11, s = 10) # Gap-segment derivative
 
-#######################
-## Second Derivative ##
-#######################
+png('dev_smoo_comp.png', width = 600, height = 1200)
+par(mfcol = c(5,1))
+plot(as.matrix(total.dev1)[1, ], type = "l", xlab = "Wavelength", ylab = "", main='raw data')
+plot(d1[1, ], type = "l", xlab = "Wavelength", ylab = "", col='green', main='first derivative')
+plot(d2[1, ], type = "l", xlab = "Wavelength", ylab = "", col='darkblue', main='second derivative')
+plot(gd1[1, ], type = "l", xlab = "Wavelength", ylab = "",col='red', main='first derivative with a gap of 10 bands')
+plot(gsd1[1, ], type = "l", xlab = "Wavelength", ylab = "",col='blue', main='Gap-segment derivative')
+dev.off()
+
+
 
 
 ##############################
