eceabba646c53e6ff6648692f2ef067a3bc7954f
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-08 10:58:29 +1100

    preprocess

--- 22_first_second_derivatives.R
+++ 22_first_second_derivatives.R
@@ -33,8 +33,6 @@ d2 <- t(diff(t(total.dev1), differences = 2)) # second derivative
 gd1 <- t(diff(t(total.dev1), differences = 1, lag = 10)) # first derivative with a gap of 10 bands
 gsd1 <- gapDer(X = total.dev1, m = 1, w = 11, s = 10) # Gap-segment derivative
 
-
-
 ##############################
 ## Savitzky-Golay filtering ##
 ##############################
@@ -54,6 +52,26 @@ plot(sg2[1, ], type = "l", col='red',xlab = "Wavelength", ylab = "", main='Savit
 plot(sg3[3, ], type = "l", col='red',xlab = "Wavelength", ylab = "", main='Savitzky-Golay 2nd derivative(5 windows, 2 poly)')
 dev.off()
 
+#############
+## Combine ##
+#############
+train_smooth <- cbind(labels, as.data.frame(gsd1)[1:1157,], train.spatial)
+test_smooth <- cbind(PIDN.test, as.data.frame(gsd1)[1158:1884,], test.spatial)
+dim(train_smooth);dim(test_smooth)
+names(train_smooth);train_smooth$Depth
+names(test_smooth);test_smooth$Depth
 
+png('test_smoothed.png', width = 1000, height = 1200)
+par(mfcol = c(1,1))
+plot(as.matrix(test_smooth)[1, -1], type = "l", xlab = "Wavelength", ylab = "", main='raw data')
+dev.off()
 
+train_smooth[which(train_smooth$Depth == 'Topsoil'),]$Depth <- 1
+train_smooth[which(train_smooth$Depth == 'Subsoil'),]$Depth <- 0
+train_smooth$Depth <- as.numeric(train_smooth$Depth)
+test_smooth[which(test_smooth$Depth == 'Topsoil'),]$Depth <- 1
+test_smooth[which(test_smooth$Depth == 'Subsoil'),]$Depth <- 0
+test_smooth$Depth <- as.numeric(test_smooth$Depth)
 
+save(d1,d2,gd1,gsd1,sg,sg2,sg3,train_smooth,test_smooth, 
+     file='data/smoothed_data_08_Oct_2014.RData')
---
+++ 24_cleaned_model.R
@@ -0,0 +1,3 @@
+setwd('/Users/ivan/Work_directory/Afr-Soil-Prediction-master')
+require(caret); require(hydroGOF); require(parcor); require(prospectr)
+load('data/smoothed_data_08_Oct_2014.RData')
