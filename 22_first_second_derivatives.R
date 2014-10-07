0cf1060402b9f67eaaba8d6ef8e05fda6040af2d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-08 08:57:42 +1100

    preprocess

--- 22_first_second_derivatives.R
+++ 22_first_second_derivatives.R
@@ -7,12 +7,26 @@ train <- train[,-co2.col]
 test <- test[,-co2.col]
 spatial.col <- 3565:3585
 spatial.col2 <- 3565:3580
+PIDN.test <- test[,1] ## 4.
 
 # spatial data for train and test
-labels <- train[,c('Ca','pH','P','SOC','Sand')]
-train.spatial <- train[,spatial.col2]
-test.spatial <- test[,spatial.col2]
-all.spatial <- rbind(train.spatial,test.spatial)
+labels <- train[,c('Ca','pH','P','SOC','Sand')] ## 1.
+train.spatial <- train[,spatial.col2] ## 2.
+test.spatial <- test[,spatial.col2] ## 3.
+all.spatial <- rbind(train.spatial,test.spatial) 
     
 # data for first derivative
 total.dev1 <- rbind(train[,-c(1,spatial.col)], test[,-c(1,spatial.col2)])
+
+######################
+## First Derivative ##
+######################
+
+#######################
+## Second Derivative ##
+#######################
+
+
+##############################
+## Savitzky-Golay filtering ##
+##############################
