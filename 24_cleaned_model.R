ca5e31b42ac5a24939791125e77d399277e19190
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-08 14:34:32 +1100

    preprocess

--- 24_cleaned_model.R
+++ 24_cleaned_model.R
@@ -10,29 +10,36 @@ train_Ca_1 <- train_Ca[index,]
 train_Ca_2 <- train_Ca[-index,]
 dim(train_Ca_1);dim(train_Ca_2)
 
+## prePro ##
+preP <- preProcess(train_Ca, method = c('center','scale'))
+train_Ca_pre <- predict(preP, train_Ca)
+plot(as.matrix(train_Ca_pre)[1,],type='l')
+plot(as.matrix(train_Ca)[1,],type='l')
+plot(as.matrix(train_smooth)[1,],type='l')
+
 ## k-fold ##
-k_fold <- createMultiFolds(train_Ca_1$TMAP,k = 10,times = 10)
+k_fold <- createMultiFolds(train_Ca$TMAP,k = 10,times = 10)
 
 ## model ##
 Grid <- expand.grid(C=c(8,24,72,216,628,1884),
                     sigma=c(0.0003,0.0118,0.03,0.1,0.3,1)) 
 
-fitControl <- trainControl(method="adaptive_cv",
+fitControl <- trainControl(method="adaptive_cv", number=10, repeats=10,
                            summaryFunction = defaultSummary,
-                           returnResamp = "all", index=k_fold,
-                           adaptive=list(min=9,alpha=.05,method='BT',complete=T))
-
-fit_Ca <- train(Ca~., data=train_Ca_1, method='svmRadial',trControl = fitControl,
+                           returnResamp = "all", 
+                           adaptive=list(min=12,alpha=.05,method='BT',complete=T))
+                            # index=k_fold)
+fit_Ca <- train(x=gsd1[1:1157,],y=labels$Ca, method='svmRadial',trControl = fitControl,
                  preProc = c('center', 'scale'),tuneLength=10,
                  verbose=T,metric='RMSE',maximize=F)
 
-fit_Ca_tune <- train(Ca~., data=train_Ca_1, method='svmRadial',
+fit_Ca_tune <- train(x=gsd1[1:1157,],y=labels$Ca, method='svmRadial',
                      trControl = fitControl,preProc = c('center', 'scale'), 
                      tune=Grid,verbose=T,metric='RMSE',maximize=F)
 
 ## prediction ##
-Ca <- predict(fit_Ca_2, test_smooth)
-rmse(Ca, train_Ca$Ca)
+Ca <- predict(fit_Ca, gsd1[1:1157,])
+rmse(Ca, train_Ca_2$Ca)
 
 fit_Ca;fit_Ca_2
 
