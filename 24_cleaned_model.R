6d6d29d51f2ba573a11852b5431115ae2e3506fd
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-08 13:34:16 +1100

    preprocess

--- 24_cleaned_model.R
+++ 24_cleaned_model.R
@@ -11,23 +11,32 @@ train_Ca_2 <- train_Ca[-index,]
 dim(train_Ca_1);dim(train_Ca_2)
 
 ## k-fold ##
-
+k_fold <- createMultiFolds(train_Ca_1$TMAP,k = 10,times = 10)
 
 ## model ##
-fitControl <- trainControl(method="adaptive_cv",number=10,
-                           repeats=5, summaryFunction = defaultSummary,
-                           returnResamp = "all",
-                           adaptive=list(min=12,alpha=.05,method='BT',complete=T))
+Grid <- expand.grid(C=c(8,24,72,216,628,1884),
+                    sigma=c(0.0003,0.0118,0.03,0.1,0.3,1)) 
 
-fit_Ca <- train(Ca~., data=train_Ca[,1:3534], method='svmRadial',trControl = fitControl,
+fitControl <- trainControl(method="adaptive_cv",
+                           summaryFunction = defaultSummary,
+                           returnResamp = "all", index=k_fold,
+                           adaptive=list(min=9,alpha=.05,method='BT',complete=T))
+
+fit_Ca <- train(Ca~., data=train_Ca_1, method='svmRadial',trControl = fitControl,
                  preProc = c('center', 'scale'),tuneLength=10,
                  verbose=T,metric='RMSE',maximize=F)
 
+fit_Ca_tune <- train(Ca~., data=train_Ca_1, method='svmRadial',
+                     trControl = fitControl,preProc = c('center', 'scale'), 
+                     tune=Grid,verbose=T,metric='RMSE',maximize=F)
+
 ## prediction ##
-Ca <- predict(fit_Ca_2, test)
+Ca <- predict(fit_Ca_2, test_smooth)
 rmse(Ca, train_Ca$Ca)
 
 fit_Ca;fit_Ca_2
 
 submit <- read.csv('submissions/submission_03Oct2014.csv', sep=',')
 head(submit); head(Ca)
+submit$Ca <- Ca
+write.csv(submit, 'submission_new/1.csv',row.names=F)
