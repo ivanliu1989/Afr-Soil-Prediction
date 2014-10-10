ea1a8dacb95a77f6c9230463db8fcef025a77335
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-10-11 09:25:25 +1100

    gbm

--- 27_Savitzky-Golay_model.R
+++ 27_Savitzky-Golay_model.R
@@ -1,12 +1,12 @@
-setwd('/Users/ivan/Work_directory/Afr-Soil-Prediction-master')
+setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Afr-Soil-Prediction')
 require(caret); require(hydroGOF); require(parcor); require(prospectr)
 load('data/Savitzky-Golay-Data.RData')
 load('data/30_Raw_Data.RData')
 
-train_P <- train_SG[,-c(1,2,4,5)] #,3559:3574
-train_P$Depth <- ifelse(train_P$Depth == 'Topsoil',1,0)
-test_P <- test_SG
-test_P$Depth <- ifelse(test_P$Depth == 'Topsoil',1,0)
+train_SOC <- train_SG[,-c(1,2,3,5)] #,3559:3574
+train_SOC$Depth <- ifelse(train_SOC$Depth == 'Topsoil',1,0)
+test_SOC <- test_SG
+test_SOC$Depth <- ifelse(test_SOC$Depth == 'Topsoil',1,0)
 y = train_P$P 
 y_array <- array(y)
 names(y_array) <- names(y)                              ## y
@@ -27,28 +27,29 @@ train_P_1 <- train_P[index,]
 train_P_2 <- train_P[-index,]
 
 # adaptive_LGOCV adaptive_cv adaptive_boot
-fitControl <- trainControl(method="adaptive_cv", number=10, repeats=5,
+fitControl <- trainControl(method="adaptive_cv", number=10, repeats=10,
                            summaryFunction = defaultSummary,
                            returnResamp = "all", selectionFunction = "best",
                            adaptive=list(min=12,alpha=.05,method='BT',complete=T))
 fitGrid <- expand.grid(C=c(2^4,2^4.25,2^4.5,2^4.75,2^5,2^5.25,2^5.5,2^5.75), 
-                       sigma=c(2^-13,2^-12,0.00028019052956,2^-11,2^-10))
+                       sigma=c(0.00028019052956, 0.000280112))
 # 0.816 baseline
 # glmStepAIC, kernelpls (0.878/1.071), plsRglm, pls (0.878/1.071)
 # spls, widekernelpls (0.855/1.071), xyf, svmRadial (0.739/0.87365)
 # svmRadial_full (0.738/0.2003)
-fit_P <- train(x=x_array, y=y, method='svmLinear',trControl = fitControl,
-               tuneLength=8, verbose=T,metric='RMSE') 
-               # preProc = c('center', 'scale'), tuneGrid = fitGrid
+fit_SOC <- train(SOC~., data=train_SOC, method='gbm',trControl = fitControl,
+               tuneLength=8, verbose=T,metric='RMSE',preProc = c('center', 'scale'))
+               , tuneGrid = fitGrid) 
+               # 
 
-P <- predict(fit_P, train_P)
+P <- predict(fit_P_linear, test_P)
 rmse(P, train_P$P)
 
 submit <- read.csv('submissions/submission_03Oct2014.csv', sep=',')
 head(submit$P); head(P)
 
 submit$P <- P
-write.csv(submit, 'submission_new/10OCT2.csv',row.names=F)
+write.csv(submit, 'submission_new/11OCT.csv',row.names=F)
 
 ## ##
 load('data/30_Raw_Data.RData')
