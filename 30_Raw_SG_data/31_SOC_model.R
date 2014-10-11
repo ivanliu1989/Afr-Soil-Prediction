15e397c78294d7f9ff5315731ce3c45e6072fa4d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-12 00:20:14 +1100

    update

--- 30_Raw_SG_data/31_SOC_model.R
+++ 30_Raw_SG_data/31_SOC_model.R
@@ -1,16 +1,22 @@
+setwd('H:\\Machine Learning\\Afr-Soil-Prediction')
 load('data/Savitzky-Golay-Data.RData')
-test_SOC <- test_SG
-train_SOC <- train_SG[,-c(1,2,3,5)] #,3559:3574
-set.seed(888)
+require(caret); require(hydroGOF); require(parcor); require(prospectr)
 
+test_Sand <- test_SG
+test_Sand$Depth <- ifelse(test_Sand$Depth == 'Topsoil',1,0)
+train_Sand <- train_SG[,-c(1,2,3,4)] #,3559:3574
+train_Sand$Depth <- ifelse(train_Sand$Depth == 'Topsoil',1,0)
+set.seed(888)
+y <- as.array(train_Sand[,1])
+x <- train_Sand[,-1]
 
 fitControl <- trainControl(method="adaptive_cv", number=10, repeats=10,
                            summaryFunction = defaultSummary,
                            returnResamp = "all", selectionFunction = "best",
                            adaptive=list(min=12,alpha=.05,method='BT',complete=T))
 
-fit_SOC <- train(SOC~., data=train_SOC, method='gbm',trControl = fitControl,
-                 tuneLength=12, verbose=T,metric='RMSE',preProc = c('center', 'scale'))
+fit_Sand <- train(Sand~.,data=train_Sand, method='svmLinear',trControl = fitControl,
+                  tuneLength=12, verbose=T,metric='RMSE',preProc = c('center', 'scale'))
 
 
 
