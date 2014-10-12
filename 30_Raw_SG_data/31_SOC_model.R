ec8331f173a398e4d474d9294941ebe0a655abd9
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-12 22:59:48 +1100

    update

--- 30_Raw_SG_data/31_SOC_model.R
+++ 30_Raw_SG_data/31_SOC_model.R
@@ -1,4 +1,4 @@
-setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Afr-Soil-Prediction\\')
+setwd('H:\\Machine Learning\\Afr-Soil-Prediction\\')
 load('data/Savitzky-Golay-Data.RData')
 require(caret); require(hydroGOF); require(parcor); require(prospectr)
 
@@ -11,13 +11,14 @@ y <- as.array(train_Sand[,1])
 x <- train_Sand[,-1]
 
 fitGrid <- expand.grid(interaction.depth=c(8,9,10),n.trees=c(300,350,400,450,500),shrinkage=c(0.05,0.1))
-fitControl <- trainControl(method="repeatedcv", number=10, repeats=10,
+fitControl <- trainControl(method="adaptive_cv", number=10, repeats=10,
                            summaryFunction = defaultSummary,
-                           returnResamp = "all", selectionFunction = "best"),
+                           returnResamp = "all", selectionFunction = "best",
+                           adaptive=list(min=12,alpha=.05,method='BT',complete=T)),
                          #  adaptive=list(min=12,alpha=.05,method='BT',complete=T))
 
-fit_SOC_tune <- train(SOC~.,data=train_SOC, method='gbm',trControl = fitControl,
-                  verbose=T,metric='RMSE',preProc = c('center', 'scale'), tuneGrid=fitGrid)
+fit_Sand <- train(Sand~.,data=train, method='gbm',trControl = fitControl,
+                  tuneLength=8,verbose=T,metric='RMSE',preProc = c('center', 'scale'), tuneGrid=fitGrid)
 # tuneLength=12, 
 
 
@@ -28,15 +29,34 @@ head(submit$SOC); head(SOC)
 
 ### e1071 ###
 require(e1071)
-fit_Sand_svm <- svm(Sand~., data=train_Sand, scale=T, type='eps-regression',
-                    kernel='linear', cost=16, cross=10,)
+
+test_Sand <- test_SG
+test_Sand$Depth <- ifelse(test_Sand$Depth == 'Topsoil',1,0)
+train_Sand <- train_SG[,-c(1,2,3,4)] #,3559:3574
+train_Sand$Depth <- ifelse(train_Sand$Depth == 'Topsoil',1,0)
+x <- train_Sand[,-1]
+y <- train_Sand$Sand
+index <- createDataPartition(train_Sand$Sand, p=.7, list=F)
+train <- train_Sand[index,]
+test <- train_Sand[-index,]
+x <- train[,-1]
+y <- train$Sand
+
+
+fit_Sand_svm <- svm(x=x, y=y, scale=T, type='eps-regression',
+                    kernel='radial', cost=10000, cross=10)
 
 tuneControl <- tune.control(nrepeat=10, sampling='cross', cross=10, best.model=T, performances=T, 
                             error.fun=T)
 
-fit_Sand_svm_tune <- best.tune(svm, Sand~., data=train_Sand, tunecontrol=tuneControl,
+fit_Sand_svm_tune <- best.tune(svm, x=x, y=y, tunecontrol=tuneControl,
                                ranges=list(gamma = 2^(-1:1),cost=2^(2:4)))
     
-    
+
+Sand <- predict(fit_Sand_svm, test[,-1])
+rmse(Sand,test$Sand)
+submit <- read.csv('submission_new/11OCT_2.csv', sep=',')
+head(submit$Sand); head(Sand)
+
 fit_SOC_2 <- fit_SOC
 load('models/SOC_26.RData')
