6bbc0c127202069a4805658344fb76d62306dd26
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-10-12 16:29:35 +1100

    update

--- 30_Raw_SG_data/31_SOC_model.R
+++ 30_Raw_SG_data/31_SOC_model.R
@@ -1,4 +1,4 @@
-setwd('H:\\Machine Learning\\Afr-Soil-Prediction')
+setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Afr-Soil-Prediction\\')
 load('data/Savitzky-Golay-Data.RData')
 require(caret); require(hydroGOF); require(parcor); require(prospectr)
 
@@ -10,17 +10,18 @@ set.seed(888)
 y <- as.array(train_Sand[,1])
 x <- train_Sand[,-1]
 
-fitControl <- trainControl(method="adaptive_cv", number=10, repeats=10,
+fitGrid <- expand.grid(interaction.depth=c(8,9,10),n.trees=c(300,350,400,450,500),shrinkage=c(0.05,0.1))
+fitControl <- trainControl(method="repeatedcv", number=10, repeats=10,
                            summaryFunction = defaultSummary,
-                           returnResamp = "all", selectionFunction = "best",
-                           adaptive=list(min=12,alpha=.05,method='BT',complete=T))
+                           returnResamp = "all", selectionFunction = "best"),
+                         #  adaptive=list(min=12,alpha=.05,method='BT',complete=T))
 
-fit_SOC <- train(SOC~.,data=train_SOC, method='gbm',trControl = fitControl,
-                  tuneLength=12, verbose=T,metric='RMSE',preProc = c('center', 'scale'))
+fit_SOC_tune <- train(SOC~.,data=train_SOC, method='gbm',trControl = fitControl,
+                  verbose=T,metric='RMSE',preProc = c('center', 'scale'), tuneGrid=fitGrid)
+# tuneLength=12, 
 
 
-
-submit <- read.csv('submissions/submission_03Oct2014.csv', sep=',')
+submit <- read.csv('submission_new/11OCT_2.csv', sep=',')
 SOC<- predict(fit_SOC, test_SOC)
 head(submit$SOC); head(SOC)
 
@@ -28,3 +29,6 @@ head(submit$SOC); head(SOC)
 
 fit_Sand_svm <- svm(Sand~., data=train_Sand, scale=T, type='eps-regression',
                     kernel='linear', cost=16, cross=10,)
+
+fit_SOC_2 <- fit_SOC
+load('models/SOC_26.RData')
