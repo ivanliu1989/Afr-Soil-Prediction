66c3c3ae1cf2e12b29e700e1686468a48e76c7b2
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-17 14:15:54 +1100

    update

--- 30_Raw_SG_data/43_manual_cv.R
+++ 30_Raw_SG_data/43_manual_cv.R
@@ -1,7 +1,7 @@
 setwd('/Users/ivan/Work_directory/Afr-Soil-Prediction-master')
 load('data/Savitzky-Golay-Data.RData')
 load('data/Savitzky-Golay-Data-Filtered.RData')
-library(caret);library(e1071);
+library(caret);library(e1071);require(hydroGOF); require(parcor); require(prospectr)
 set.seed(888);
 trainingdata <- train_SG;
 testdata <- test_SG;
@@ -13,7 +13,7 @@ test$Depth <- with ( test, ifelse ( ( Depth == 'Subsoil' ), 0 , 1 ) );
 pred_cols = trainingdata[, soil_properties];
 
 #now just change the p_var to some of the soil properties
-p_var = "pH";
+p_var = "Ca";
 folds = createFolds(pred_cols[,p_var],k=10);
 fold_rmse = rep(0,10);
 avg_rmse = 0;
@@ -25,9 +25,12 @@ for(i in 1:length(folds)){
     g_y_test = pred_cols[smpl,p_var];
     m2 = svm(x=as.matrix(g_train),y=g_y,scale=T,kernel="radial",cost=36);
     m2.pred = predict(m2,newdata=g_test,type="response");
-    fold_rmse[i]=sqrt(mean((m2.pred - g_y_test)^2));
+    fold_rmse[i]=rmse(m2.pred, g_y_test);
 }
 mean(fold_rmse);
 sd(fold_rmse);
 
-# 36 (0.308/0.038)
+# Ca 36 Radial noScale (0.048/0.009)
+# Ca 36 Radial Scale (0.048/0.009)
+# Ca 36 Linear noScale ()
+# Ca 36 Linear Scale ()
